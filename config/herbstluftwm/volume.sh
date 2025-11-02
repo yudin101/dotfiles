81e608a8b81489d7f5ea4738705dc82d0a1f2608
yudin101 <contact@yudin101.com.np>
2025-11-02 18:41:02 +0545

    pulseaudio > pipewire

--- config/herbstluftwm/volume.sh
+++ config/herbstluftwm/volume.sh
@@ -2,20 +2,33 @@
 
 case $1 in
     down)
-        # amixer -q set Master 10%- unmute
-        pactl set-sink-volume @DEFAULT_SINK@ -10%
+        amixer -q set Master 10%- unmute
         herbstclient emit_hook volume
         ;;
     up)
-        # amixer -q set Master 10%+ unmute
-        pactl set-sink-volume @DEFAULT_SINK@ +10%
+        amixer -q set Master 10%+ unmute
         herbstclient emit_hook volume
         ;;
     mute)
-        # amixer -q set Master toggle
-        pactl set-sink-mute @DEFAULT_SINK@ toggle
+        amixer -q set Master toggle
         herbstclient emit_hook volume_toggle
         ;;
 esac
 
-amixer get Master | grep '%' | dzen2 -p 1
+# amixer get Master | grep '%' | dzen2 -p 1
+
+# get volume percent
+vol=$(amixer get Master | grep -oP '\[\d+%\]' | head -1 | tr -d '[]')
+
+# check mute status
+mute=$(amixer get Master | grep -oP '\[(on|off)\]' | head -1 | tr -d '[]')
+
+# decide what to show
+if [ "$mute" = "off" ]; then
+    display="Volume: Muted"
+else
+    display="Volume: $vol"
+fi
+
+# show in dzen2
+echo "$display" | dzen2 -p 1 
