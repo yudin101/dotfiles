819409c8c9bf765a6cba71f61f8d843e611b8104
yudin101 <contact@yudin101.com.np>
2026-02-25 02:09:53 +0545

    teide theme change

--- config/nvim/lua/plugins/teide.lua
+++ config/nvim/lua/plugins/teide.lua
@@ -2,5 +2,17 @@ return {
 	"serhez/teide.nvim",
 	lazy = false,
 	priority = 1000,
-	opts = {},
+	opts = {
+		style = "darker",
+		on_colors = function(colors)
+			colors.bg = "#000000"
+		end,
+		on_highlights = function(highlights, colors)
+			highlights.TelescopeNormal = { bg = colors.bg }
+			highlights.TelescopePromptNormal = { bg = colors.bg }
+			highlights.TelescopePromptBorder = { bg = colors.bg, fg = colors.fg_gutter }
+			highlights.TelescopeResultsNormal = { bg = colors.bg }
+			highlights.TelescopeResultsBorder = { bg = colors.bg, fg = colors.fg_gutter }
+		end,
+	},
 }
